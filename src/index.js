e9133cae0efbf6e2c31e93b11efa11b3a3419790
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-09 05:01:33 +0600

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import './style.css';
 import App from './App';
 import store from './store/store';
 
